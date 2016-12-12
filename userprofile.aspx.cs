2d0c1b74689fa4f2a644b62e3c7672bbddeaa9df
pasindu7452 <math7452@gmail.com>
2016-12-12 12:53:44 +0530

    retireving user profie info and displayin them in user profile

--- userprofile.aspx.cs
+++ userprofile.aspx.cs
@@ -6,13 +6,31 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Configuration;
 
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         lblLoggedin.Text ="you are logged in as :" + Convert.ToString(Session["username"]);
+
+        string cs = ConfigurationManager.ConnectionStrings["dbex"].ConnectionString;
+        
+        using(SqlConnection con = new SqlConnection(cs))
+        {
+            con.Open();
+            SqlCommand cmd=new SqlCommand("SELECT * FROM tblUser ",con);
+            using( SqlDataReader dr=cmd.ExecuteReader())
+            {
+                GridView1.DataSource = dr;
+                GridView1.DataBind();
+            }
+
+        }
+
+        
+
+
 
 
     }
