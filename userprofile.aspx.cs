0760d3b112bf8c1b4f57a68b5a8e6fddab5c1efd
pasindu7452 <math7452@gmail.com>
2016-12-12 12:00:02 +0530

    added namespaces for sqlclient

--- userprofile.aspx.cs
+++ userprofile.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
 
 public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
         lblLoggedin.Text ="you are logged in as :" + Convert.ToString(Session["username"]);
+
+
     }
 }
