ab9915777fe8cd2064aec4a39eeeabf6a4325384
pasindu7452 <math7452@gmail.com>
2017-01-12 15:57:56 +0530

    add new page call comments and developed code to store user comments

---
+++ comments.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="comments.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <title>comments</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CarouselPlaceHolder" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PageBody1" Runat="Server">
+
+    <form id="form1" runat="server">
+        <div class="col-md-12 text-right">
+            <asp:Label ID="lblLoggedin" runat="server"></asp:Label> <br />
+
+            <asp:Button ID="btnLogout" runat="server" Text="Logout" OnClick="btnLogout_Click" />
+        </div>
+        
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+
+                <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+
+               <div class="form-group">
+                            <asp:Label ID="lblComment" runat="server" Text="Please type your Comment:"></asp:Label>
+                              <textarea class="form-control" rows="5" id="txtComment" runat="server"></textarea>
+                               <br />
+                            <asp:Button ID="btnComment" runat="server" Text="Submit" CssClass="btn btn-primary btn-lg" OnClick="btnComment_Click" />
+               </div>
+            </div>
+        </div>
+    </form>
+
+</asp:Content>
+
---
+++ comments.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["username"] == null)
+        {
+            Response.Redirect("~/user_signin.aspx");
+        }
+        else
+        {
+            lblLoggedin.Text = "you are logged in as :" + Convert.ToString(Session["username"]);
+            string user = Convert.ToString(Session["username"]);
+        }
+    }
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        Session["username"] = null;
+        if (Session["username"] == null)
+        {
+            Response.Redirect("~/user_signin.aspx");
+
+        }
+    }
+
+    protected void btnComment_Click(object sender, EventArgs e)
+    {
+        string datetime = DateTime.Now.ToString();
+        string user = Convert.ToString(Session["username"]);
+
+        if (txtComment.InnerText == "")
+        {
+            lblMessage.Text = "Please enter your comment";
+        }
+        else
+        {
+            string cs = ConfigurationManager.ConnectionStrings["dbex"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spAddcomment",con);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@email", user);
+                cmd.Parameters.AddWithValue("@date",datetime);
+                cmd.Parameters.AddWithValue("@comment", txtComment.InnerText);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                lblMessage.Text = "your comment has successfully saved in database.";
+
+            }
+
+        }
+    }
+}
