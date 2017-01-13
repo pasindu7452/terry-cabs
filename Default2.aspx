cbfbcf9bd134c8139176d0311383772a0fa6202e
pasindu7452 <math7452@gmail.com>
2017-01-13 14:09:02 +0530

    done chaneges

---
+++ Default2.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Default2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <title>signup</title>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CarouselPlaceHolder" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="PageBody1" Runat="Server">
+
+     <form runat="server">
+         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+
+         </form>
+    
+</asp:Content>
+
---
+++ Default2.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Security;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbex"].ConnectionString;
+
+        using (SqlConnection con = new SqlConnection(cs))
+        {
+            SqlCommand cmd = new SqlCommand("spAdmin",con);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            string pass = FormsAuthentication.HashPasswordForStoringInConfigFile(TextBox4.Text,"SHA1");
+
+            cmd.Parameters.AddWithValue("@first",TextBox1.Text);
+            cmd.Parameters.AddWithValue("@last", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@email", TextBox3.Text);
+            cmd.Parameters.AddWithValue("@pass", pass);
+            con.Open();
+            cmd.ExecuteNonQuery();
+
+
+
+
+
+        }
+    }
+}
